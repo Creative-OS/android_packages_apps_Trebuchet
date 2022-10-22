crdroidandroid/android_packages_apps_Launcher3

Launcher3: Add toggle disable hotseat searchbar
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/Hotseat.java
@@ -62,7 +62,11 @@ public class Hotseat extends CellLayout implements Insettable {
     public Hotseat(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        if (Utilities.isQSBEnabled(context)) {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        } else {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat_noqsb, this, false);
+        }
         mQsbHeight = mQsb.getLayoutParams().height;
         addView(mQsb);
 

Creative-OS/android_packages_apps_Trebuchet

Signed-off-by: str-br <alexfinhart@gmail.com>
Change-Id: I75b492a8b90001a60f0ca95d6e3cc088611e9a1e

src/com/android/launcher3/Hotseat.java
@@ -60,7 +60,11 @@ public class Hotseat extends CellLayout implements Insettable {
     public Hotseat(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        if (Utilities.showQSB(context)) {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        } else {
+            mQsb = LayoutInflater.from(context).inflate(R.layout.empty_view, this, false);
+        }
         addView(mQsb);
 
         mQsbHeight = getResources().getDimensionPixelSize(R.dimen.qsb_widget_height);